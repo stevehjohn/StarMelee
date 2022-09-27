552eb54795ffa5da83d216ea08864b97441271c2
Stevö <stevehuwjohn@gmail.com>
2022-09-27 17:45:11 +0100

    WIP

---
+++ src/StarMelee/GameElements/Ship.cs
@@ -0,0 +1,14 @@
+﻿using StarMelee.Geometry;
+
+namespace StarMelee.GameElements
+{
+    public class Ship
+    {
+        public Position<float> Position { get; }
+
+        public Ship(Position<float> startPosition)
+        {
+            Position = startPosition;
+        }
+    }
+}
---
+++ src/StarMelee/Geometry/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StarMelee.Geometry
+{
+    public class Position<T> where T : struct, IComparable<T>
+    {
+        public T X { get; }
+
+        public T Y { get; }
+
+        public Position(T x, T y)
+        {
+            X = x;
+
+            Y = y;
+        }
+    }
+}
