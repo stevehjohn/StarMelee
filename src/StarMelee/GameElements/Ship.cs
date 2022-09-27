78f69260d52c38300d1c305640a453c229d710b3
Stevö <stevehuwjohn@gmail.com>
2022-09-27 17:51:30 +0100

    WIP

--- src/StarMelee/GameElements/Ship.cs
+++ src/StarMelee/GameElements/Ship.cs
@@ -6,9 +6,17 @@ namespace StarMelee.GameElements
     {
         public Position<float> Position { get; }
 
+        public float Direction { get; set; }
+
+        public float Speed { get; set; }
+
         public Ship(Position<float> startPosition)
         {
             Position = startPosition;
+
+            Direction = 0;
+
+            Speed = 0;
         }
     }
 }
---
+++ src/StarMelee/Infrastructure/GameConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace StarMelee.Infrastructure
+{
+    public class GameConstants
+    {
+        public const int ArenaWidth = 1920 * 8;
+
+        public const int ArenaHeight = 1080 * 8;
+    }
+}
